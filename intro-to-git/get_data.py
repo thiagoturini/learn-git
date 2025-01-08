386733ab864521d2236eeb50e29aa85092c31866
Nuno <nscurado@gmail.com>
2025-01-08 22:43:15 +0000

    Change the data

--- intro-to-git/get_data.py
+++ intro-to-git/get_data.py
@@ -6,7 +6,8 @@ data = [{"product": "tennis Adidas", "quatity": 1, "value": 120.99},
         {"product": "sticker", "quatity": 1, "value": 1.99},
         {"product": "socks", "quatity": 1, "value": 10.99},
         {"product": "socks WWW", "quatity": 1, "value": 20.99},
-        {"product": "tennis ZZZZ", "quatity": 1, "value": 120.99}]
+        {"product": "tennis ZZZZ", "quatity": 1, "value": 120.99},
+        {"product": "tennis XXXX", "quatity": 1, "value": 99.99}]
 
 
 
@@ -14,6 +15,7 @@ df = pd.DataFrame(data)
 print(df)
 min_value = df['value'].min()
 max_value = df['value'].max()
+mean_value = df['value'].mean()
 
 Q3, Q1 = np.percentile(df['value'], [75, 25])
 IQR = Q3 - Q1
